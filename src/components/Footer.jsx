5f2dd5532be68727e1033a25dd1b598808383875
Devan Prabowo <s11810160@student.unklab.ac.id>
2023-12-04 04:07:09 +0800

    Devan/Ken-Footer Message

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,41 +9,49 @@ const Footer = () => {
                   <div className="col-md-4 site-animate">
                     <h2 className="site-heading-2">About Us</h2>
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                      Cumque, similique, delectus blanditiis odit expedita amet.
-                      Sed labore ipsum vel dolore, quis, culpa et magni autem
-                      sequi facere eos tenetur, ex?
+                      This is a Final Project for our Front-End Web Development class, this is a projet to introduce some of Indonesian traditional cuisine.
+                      We hope that this website would be able to help people that want to know some of Indonesian cuisine.
                     </p>
                   </div>
                   <div className="col-md-1" />
                   <div className="col-md-3 site-animate">
                     <div className="site-footer-widget mb-4">
-                      <h2 className="site-heading-2">The Restaurant</h2>
+                      <h2 className="site-heading-2">The Development Team Member</h2>
                       <ul className="list-unstyled">
                         <li>
-                          <a href="#" className="py-2 d-block">
-                            About Us
+                          <a href="https://www.instagram.com/gabyloing/" className="py-2 d-block">
+                            Gaby Loing
                           </a>
                         </li>
                         <li>
-                          <a href="#" className="py-2 d-block">
-                            Chefs
+                          <a href="https://www.instagram.com/briannalxndraa/" className="py-2 d-block">
+                            Brianna Rhebok
                           </a>
                         </li>
                         <li>
-                          <a href="#" className="py-2 d-block">
-                            Events
+                          <a href="https://www.instagram.com/vicananana_/" className="py-2 d-block">
+                            Veronica Tauran
                           </a>
                         </li>
                         <li>
-                          <a href="#" className="py-2 d-block">
-                            Contact
+                          <a href="https://www.instagram.com/koyanagiyukiii/" className="py-2 d-block">
+                            Koyanagi Yuki
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://www.instagram.com/betahkenrick/" className="py-2 d-block">
+                            Kendrick Betah
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://www.instagram.com/devanprabowo1/" className="py-2 d-block">
+                            Devan Prabowo
                           </a>
                         </li>
                       </ul>
                     </div>
                   </div>
-                  <div className="col-md-2 site-animate">
+                  {/* <div className="col-md-2 site-animate">
                     <div className="site-footer-widget mb-4">
                       <h2 className="site-heading-2">Useful links</h2>
                       <ul className="list-unstyled">
@@ -74,8 +82,8 @@ const Footer = () => {
                         </li>
                       </ul>
                     </div>
-                  </div>
-                  <div className="col-md-2 site-animate">
+                  </div> */}
+                  {/* <div className="col-md-2 site-animate">
                     <div className="site-footer-widget mb-4">
                       <h2 className="site-heading-2">Useful links</h2>
                       <ul className="list-unstyled">
@@ -106,12 +114,12 @@ const Footer = () => {
                         </li>
                       </ul>
                     </div>
-                  </div>
+                  </div> */}
                 </div>
               </div>
             </div>
             <div className="row site-animate">
-              <div className="col-md-12 text-center">
+              {/* <div className="col-md-12 text-center">
                 <div className="site-footer-widget mb-4">
                   <ul className="site-footer-social list-unstyled ">
                     <li className="site-animate">
@@ -131,7 +139,7 @@ const Footer = () => {
                     </li>
                   </ul>
                 </div>
-              </div>
+              </div> */}
               <div className="col-md-12 text-center">
                 <p>
                   ©{" "}
