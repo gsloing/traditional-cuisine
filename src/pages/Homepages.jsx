f4e9f3298007b8db15c858401d2a1e46d83c850e
Devan Prabowo <s11810160@student.unklab.ac.id>
2023-12-03 18:29:11 +0800

    Header Comp

--- src/pages/Homepages.jsx
+++ src/pages/Homepages.jsx
@@ -1,10 +1,12 @@
 import About from "../components/About";
 import Gallery from "../components/gallery/Gallery";
+import Header from "../components/Header";
 import List from "../components/List";
 import ListMakanan from "../components/listMakanan/ListMakanan";
 const Homepages = () => {
   return (
     <div>
+      <Header/>
       <About/>
       <Gallery/>
       <List />
