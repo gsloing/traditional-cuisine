170829b791d1d64e48ea3d86a798a174dd26afa3
gsloing <gabysloing@gmail.com>
2023-12-04 09:18:45 +0800

    Update

---
+++ src/pages/Homepages.jsx
@@ -0,0 +1,20 @@
+import List from "../components/List";
+import About from "../components/About";
+import Footer from "../components/Footer";
+import Gallery from "../components/gallery/gallery";
+import Header from "../components/Header";
+import ListMakanan from "../components/listMakanan/listMakanan";
+const Homepages = () => {
+  return (
+    <div>
+      <Header/>
+      <About/>
+      <Gallery/>
+      <ListMakanan/>
+      <List />
+      <Footer/>
+    </div>
+  );
+};
+
+export default Homepages;
